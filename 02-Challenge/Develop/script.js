f33279b553ccbed1f03bf632a5d948c576d20626
Chris <cmizelle10@gmail.com>
2022-10-03 22:33:39 -0400

    created generatePassword function; prompted user for password length

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,6 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Create generatePassword function
+function generatePassword() {
+  
+  while (true) {
+    var userInput = window.prompt("How many characters would you like your password to be?")
+    var passwordLength = parseInt(userInput)
+    if (isNaN(passwordLength)) {
+      window.alert("Please enter a numeric value for password length")
+    } else if (passwordLength < 8 || passwordLength > 128) {
+      window.alert("Password length must be between 8 and 128 characters")
+    } else {
+      break
+    }
+  }
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -8,7 +25,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+} 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
